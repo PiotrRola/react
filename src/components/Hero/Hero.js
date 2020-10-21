be052a240f2b80a1520402c2f1560a20fbd74720
Piotr Rola <rolapiotr@interia.pl>
2020-10-21 21:23:52 +0200

    add Column.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import styles from './Hero.scss';
+import PropTypes from 'prop-types';
 
-class Hero extends React.Component {
-  render() {
-    return (
+const Hero = props =>(
+        
       <header className={styles.component}>
         
-        <h2 className={styles.title}>things to do!</h2>
-        <img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png" ></img>
+        <h2 className={styles.title}>{props.titleText}</h2>
+        <img className={styles.image} src={props.image}></img>
         
       </header>
     )
-  }
-}
+  
+  
 
+Hero.propTypes = {
+titleText: PropTypes.node.isRequired  ,
+
+}
 export default Hero;
